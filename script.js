77a30a28511c9178a4929ba9d99d6dec41f23069
Rushyyprajapati <145515777+Rushyyprajapati@users.noreply.github.com>
2024-02-03 16:35:04 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function reserveBook() {
+    // Assuming you would make an asynchronous request to reserve the book
+    // For demonstration purposes, an alert is used here.
+    alert('Book Reserved!');
+}
