78e51bd6f5cb6220484baba8524c9ce3e4bb33f5
Rushyyprajapati <145515777+Rushyyprajapati@users.noreply.github.com>
2024-02-11 21:01:04 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ function reserveBook() {
     // For demonstration purposes, an alert is used here.
     alert('Book Reserved!');
 }
+function toggleDropdown() {
+    var dropdownMenu = document.getElementById("dropdownMenu");
+    if (dropdownMenu.style.display === "none" || dropdownMenu.style.display === "") {
+        dropdownMenu.style.display = "block";
+    } else {
+        dropdownMenu.style.display = "none";
+    }
+}
+function toggleCart() {
+    var cartLogo = document.getElementById("cartLogo");
+    cartLogo.classList.toggle("active-cart");
+}
